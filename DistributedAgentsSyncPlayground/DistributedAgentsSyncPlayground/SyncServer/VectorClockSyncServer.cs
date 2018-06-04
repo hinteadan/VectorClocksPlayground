2edaf720dbdcaedd6e323b6edb717759664ad10c
Hintee <hintea_dan@yahoo.co.uk>
2018-06-05 00:41:46 +0300

    OD: Sync Server

---
+++ DistributedAgentsSyncPlayground/DistributedAgentsSyncPlayground/SyncServer/VectorClockSyncServer.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Concurrent;
+
+namespace DistributedAgentsSyncPlayground.SyncServer
+{
+    public class VectorClockSyncServer<T>
+    {
+        private readonly ConcurrentQueue<T> syncRequestQueue 
+    }
+}

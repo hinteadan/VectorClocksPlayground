597d40b51bbf59b75a54aae3ffc63731f857c988
Hintee <hintea_dan@yahoo.co.uk>
2018-06-07 22:39:06 +0300

    Refactor to separate assemblies

---
+++ DistributedAgentsSyncPlayground/H.VectorClocks.Http/HttpVectorClockNode.cs
@@ -0,0 +1,30 @@
+﻿using H.VectorClocks;
+using Nancy.Hosting.Self;
+using System;
+
+namespace DistributedAgentsSyncPlayground.Http
+{
+    public class HttpVectorClockNode : VectorClockNode<string>, IDisposable
+    {
+        private readonly NancyHost httpServer = new NancyHost();
+
+        #region Construct
+        public HttpVectorClockNode(string url) : this(url, null)
+        {
+            
+        }
+
+        public HttpVectorClockNode(string url, string payload, params VectorClockNodeVersion[] revision) : base(url, payload, revision)
+        {
+            httpServer = new NancyHost(new Uri(url));
+            httpServer.Start();
+        }
+        #endregion
+
+        public void Dispose()
+        {
+            httpServer.Stop();
+            httpServer.Dispose();
+        }
+    }
+}
